91be121b169740edb4fd0bd47b1df9d49e5b0a4c
Tony Keiser <keisertony@gmail.com>
2023-01-26 12:05:17 -0800

    File watching

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 #!/usr/bin/env node
+const debounce = require('lodash.debounce')
+const chokidar = require('chokidar')
+const program = require('caporal')
+const fs = require('fs')
 
-console.log('Hello there! 👋')
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute. 😵')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js'
+
+    try {
+      await fs.promises.access(name)
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`)
+    }
+
+    const start = debounce(() => {
+      console.log("Starting User's Program!")
+    }, 100)
+
+    chokidar
+      .watch('.')
+      .on('add', () => start)
+      .on('change', () => start)
+      .on('unlink', () => start)
+  })
+
+program.parse(process.argv)
